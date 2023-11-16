2c77f52444b2c7bfe4da716468046b858f47a9f2
Patricia Rocha <patirocha.dev@gmail.com>
2023-11-15 21:26:32 -0300

    projeto criado

---
+++ src/main/java/com/patriciarocha/pointrecordsapi/PointrecordsapiApplication.java
@@ -0,0 +1,13 @@
+package com.patriciarocha.pointrecordsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointrecordsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointrecordsapiApplication.class, args);
+	}
+
+}
